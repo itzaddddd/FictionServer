042b73612148aa2689940d8fd74e31bb733593d7
itzaddddd <isarafff@gmail.com>
2021-04-21 00:39:03 +0700

    update test get api

--- src/controllers/fiction.controller.ts
+++ src/controllers/fiction.controller.ts
@@ -1,12 +1,21 @@
 import 'reflect-metadata'
 import { Context } from 'koa'
 import { Controller, Body, Ctx, Get, Post } from 'routing-controllers'
-import { predictFiction } from '../services/fiction.service';
+import { predictFiction, testFiction } from '../services/fiction.service';
 import { IPredict } from '../interfaces/input.interface';
 import { httpOk, httpInternalServerError } from './utils/responseHelper'
 
 @Controller('/fic')
 export default class fictionController{
+    @Get('/')
+    async testFiction(@Ctx() ctx: Context){
+        const result = await testFiction()
+        if(!result) httpInternalServerError(ctx, {msg: 'Server Error'})
+        httpOk(ctx, result)
+        return ctx.response
+    }
+
+
     @Post('/')
     async predictFiction(@Ctx() ctx: Context, @Body() request: IPredict){
         const result = await predictFiction(request)
--- src/services/fiction.service.ts
+++ src/services/fiction.service.ts
@@ -2,6 +2,10 @@ import * as tf from '@tensorflow/tfjs-node'
 import { IPredict } from '../interfaces/input.interface'
 
 const MODEL_PATH = 'file://src/tensorflow/model.json'
+
+export const testFiction = async (): Promise<string> => {
+    return 'Test Fiction Gene API'
+}
 export const predictFiction = async (request: IPredict): Promise<any> => {
     const model = await tf.loadLayersModel(MODEL_PATH)
     const inputValue = request.input
